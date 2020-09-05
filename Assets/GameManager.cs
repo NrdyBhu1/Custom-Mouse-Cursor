af5773a0354b6ea82a1e1bbc861821d0d3179324
NrdyBhu1 <61649925+NrdyBhu1@users.noreply.github.com>
2020-09-05 19:10:50 +0530

    Add files via upload

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Variables
+    public Texture2D pointer;
+    public Texture2D hand;
+    #endregion
+
+    void Start()
+    {
+        setPointer();
+    }
+
+    public void setHand(){
+        Cursor.SetCursor(hand, new Vector2(0, 0), CursorMode.ForceSoftware);
+    }
+
+    public void setPointer(){
+        Cursor.SetCursor(pointer, new Vector2(0, 0), CursorMode.ForceSoftware);
+    }
+}
